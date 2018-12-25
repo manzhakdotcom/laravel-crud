70ba68b4ff18ca168cc8ecc510e2322d71626bf0
Sergey Manzhak <manzhak.com@gmail.com>
2018-12-25 15:13:18 +0300

    Initial commit

---
+++ app/Http/Controllers/PassportController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PassportController extends Controller
+{
+    public function index()
+    {
+        $passports = \App\Passport::all();
+        return view('index', compact('passports'));
+    }
+
+    //
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        if($request->hasFile('filename')) {
+            $file = $request->file('filename');
+            $name = time() . '-' . $file->getClientOriginalName();
+            $file->move(public_path() . '/images/', $name);
+        } else {
+            return redirect('passports/create')->with('warning', 'Select file.');
+        }
+        $passport = new \App\Passport;
+        $passport->name = $request->get('name');
+        $passport->email = $request->get('email');
+        $passport->number = $request->get('number');
+        $date = date_create($request->get('date'));
+        $format = date_format($date, 'Y-m-d');
+        $passport->date = strtotime($format);
+        $passport->office = $request->get('office');
+        $passport->filename = $name;
+        $passport->save();
+
+        return redirect('passports')->with('success', 'information has een added');
+    }
+
+    public function edit($id)
+    {
+        $passport = \App\Passport::find($id);
+        return view('edit', compact('passport', 'id'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $passport = \App\Passport::find($id);
+        $passport->name = $request->get('name');
+        $passport->email = $request->get('email');
+        $passport->number = $request->get('number');
+        $passport->office = $request->get('office');
+        $passport->save();
+
+        return redirect('passports');
+    }
+
+    public function destroy($id)
+    {
+        $passport = \App\Passport::find($id);
+        $passport->delete();
+        return redirect('passports')->with('success', 'Information has been deleted');
+    }
+}
---
+++ resources/views/create.blade.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.min.css"
+          rel="stylesheet">
+
+    <title>Create</title>
+</head>
+<body>
+<div class="container">
+    <br />
+    @if (\Session::has('warning'))
+        <div class="alert alert-warning">
+            <p>{{ \Session::get('warning') }}</p>
+        </div><br />
+    @endif
+    <h2>Passport Appointment System</h2><br/>
+    <form method="post" action="{{url('passports')}}" enctype="multipart/form-data">
+        @csrf
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+                <label for="Name">Name:</label>
+                <input type="text" class="form-control" name="name">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+                <label for="Email">Email:</label>
+                <input type="text" class="form-control" name="email">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+                <label for="Number">Phone Number:</label>
+                <input type="text" class="form-control" name="number">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+                <input type="file" name="filename">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+                <strong>Date : </strong>
+                <input class="date form-control" type="text" name="date" data-toggle="datepicker">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+                <lable>Passport Office</lable>
+                <select name="office">
+                    <option value="Mumbai">Mumbai</option>
+                    <option value="Chennai">Chennai</option>
+                    <option value="Delhi">Delhi</option>
+                    <option value="Bangalore">Bangalore</option>
+                </select>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4" style="margin-top:60px">
+                <button type="submit" class="btn btn-success">Submit</button>
+            </div>
+        </div>
+    </form>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.min.js"></script>
+<script type="text/javascript">
+    $(function() {
+        $('[data-toggle="datepicker"]').datepicker({
+            autoHide: true,
+            zIndex: 2048,
+        });
+    });
+</script>
+</body>
+</html>
