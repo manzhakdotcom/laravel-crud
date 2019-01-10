09bc19ed9fef732ceb2ab3eade8eb5a1e6e9fb41
Sergey Manzhak <manzhak.com@gmail.com>
2019-01-10 16:55:05 +0300

    Work with models

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@
 //Появился с 5.5
 //Route::view('/', 'welcome');
 //Route::redirect('/here1', 'passports', 301);
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'UsersController@index');
 
 Route::get('/{name}/{id}', function ($name, $id) {
     return view('new/welcome', compact('name'))->withIdName($id);
